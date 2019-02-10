c63f75a2b26167f24bca010a6f23f85dba3f5f7a
sdbowen <sdbowen.git@gmail.com>
2019-02-10 10:43:46 -0700

    Remove devise registration paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :trips
-  devise_for :users
+  devise_for :users, skip: [:registration]
   root to: 'static#homepage'
 end
