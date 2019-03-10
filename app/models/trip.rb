7de2c67f65eab6b57dfe1cbccc7389f1da94e434
sdbowen <sdbowen.git@gmail.com>
2019-03-10 15:59:57 -0600

    Prevent empty strings from being saved in trip db

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,6 +6,7 @@ class Trip < ApplicationRecord
 
   enum status: { entered: 0, scheduled: 1, completed: 2, billed: 3, locked: 4 }
   validates :first_name, :last_name, :pickup_address, :pickup_city, :pickup_zip, :delivery_address, :delivery_city, presence: true
+  before_save :normalize_blank_values
 
   scope :assigned_to, ->(user) { where(user_id: user.id) unless (user.role & %w[dispatch admin]).present? }
   scope :scheduled_on, ->(date) { where(scheduled_pickup_date: date) }
@@ -29,6 +30,12 @@ class Trip < ApplicationRecord
       "<h5>Special Needs</h5><p>#{special_needs}</p>"
     else
       "<h5>Special Needs</h5><p>#{special_needs}</p> <br/> <h5>Instructions</h5> <p>#{instructions}</p>"
+    end
+  end
+
+  def normalize_blank_values
+    attributes.each do |column, _value|
+      self[column].present? || self[column] = nil
     end
   end
 end
