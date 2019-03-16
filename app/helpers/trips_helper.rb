820f9c1c72b6f5fead48c8f6f5cc6df19156b472
sdbowen <sdbowen.git@gmail.com>
2019-03-15 20:42:50 -0600

    Add address links to google maps

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -5,6 +5,18 @@ module TripsHelper
     status_span_generator status
   end
 
+  def build_url(trip, type)
+    if type == 'pickup'
+      url = 'https://www.google.com/maps/search/?api=1&query='
+      query = "#{trip.pickup_address}, #{trip.pickup_city}, #{trip.pickup_zip}"
+      url + CGI.escape(query)
+    else
+      url = 'https://www.google.com/maps/search/?api=1&query='
+      query = "#{trip.delivery_address}, #{trip.delivery_city}"
+      url + CGI.escape(query)
+    end
+  end
+
   private
 
   def status_span_generator(status)
